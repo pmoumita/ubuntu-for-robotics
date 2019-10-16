ec9706925cf8d9ed0c8092dc2830b60d2852b7d8
Abhishek Kathpal <akathpal@umd.edu>
2019-10-16 14:17:15 -0400

    updated_killer_wifi_script

--- LatestScripts/install_wifi_driver.sh
+++ LatestScripts/install_wifi_driver.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # Install WiFi Drivers
+
+sudo apt-get install -y build-essential
+
+# Download the Iwlwifi-Firmware.git repository
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+cd linux-firmware
+sudo cp iwlwifi-* /lib/firmware/
+cd ..
+
+# Download the Backport-Iwlwifi.git repository
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/backport-iwlwifi.git
 cd backport-iwlwifi
-make defconfig-iwlwifi-public
-sed -i 's/CPTCFG_IWLMVM_VENDOR_CMDS=y/# CPTCFG_IWLMVM_VENDOR_CMDS is not set/' .config
-make -j4
+sudo make defconfig-iwlwifi-public
+sudo make -j4
 sudo make install
+update-initramfs -u
+sudo reboot
 # on kernel update do sudo make install again
 
